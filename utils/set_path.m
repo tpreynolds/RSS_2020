045f6e8ee07958de899566004361cc800032b2c9
Taylor Reynolds <tpr6@uw.edu>
2020-05-16 17:05:20 -0700

    updated the set path to stop if Yalmip/Mosek are not yet installed, will add them as submodules later

--- utils/set_path.m
+++ utils/set_path.m
@@ -5,17 +5,26 @@ addpath(genpath('../utils/'))
 addpath('../classes/')
 
 % add the Yalmip files
-yalmiproot = '../../Desktop/YALMIP-master/';
-addpath(yalmiproot)
-addpath(strcat(yalmiproot,'extras'))
-addpath(strcat(yalmiproot,'solvers'))
-addpath(strcat(yalmiproot,'modules'))
-addpath(strcat(yalmiproot,'modules/parametric'))
-addpath(strcat(yalmiproot,'modules/moment'))
-addpath(strcat(yalmiproot,'modules/global'))
-addpath(strcat(yalmiproot,'modules/sos'))
-addpath(strcat(yalmiproot,'operators'))
-clear yalmiproot
+try
+    yalmiproot = '../../Desktop/YALMIP-master/';
+    addpath(yalmiproot)
+    addpath(strcat(yalmiproot,'extras'))
+    addpath(strcat(yalmiproot,'solvers'))
+    addpath(strcat(yalmiproot,'modules'))
+    addpath(strcat(yalmiproot,'modules/parametric'))
+    addpath(strcat(yalmiproot,'modules/moment'))
+    addpath(strcat(yalmiproot,'modules/global'))
+    addpath(strcat(yalmiproot,'modules/sos'))
+    addpath(strcat(yalmiproot,'operators'))
+    clear yalmiproot 
+catch 
+    fprintf('Did not find a YALMIP installation on the Desktop, stopping')
+    return;
+end
 
 % add the mosek files
-addpath(genpath('../../Desktop/mosek/9.1/toolbox/r2015a/'))
+try
+    addpath(genpath('../../Desktop/mosek/9.1/toolbox/r2015a/'))
+catch
+    fprintf('Did not find a MOSEK installation on the Desktop, stopping')
+end
